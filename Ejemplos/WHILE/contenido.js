97fd8f7bf9f29d03d4f5506643e53d93c4fedb28
jorgepechg <jorgepechgrajales@gmail.com>
2020-12-18 14:45:51 -0500

    learning

---
+++ Ejemplos/WHILE/contenido.js
@@ -0,0 +1,10 @@
+var cantidad = 0;
+//crear el indice
+var i = 1;
+//crear la condicion
+while ( i <= 10 ){
+    cantidad = cantidad + 100;
+    //incrementar el indice
+    i++;
+}
+alert("El valor final es " + cantidad);
